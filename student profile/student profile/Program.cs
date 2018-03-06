1bed008e23d767e2b6c1a1548e8f8c8eba3d0224
ibrahim <ibrahim.ramzan069@gmail.com>
2018-03-06 22:01:18 +0500

    !Assignment#01 (Student_profile)

---
+++ student profile/student profile/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace student_profile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int user_option;
+            string line = null;
+            Student student_Object = new Student();
+            while (true)
+            {
+
+                Console.WriteLine("1: Create Student profile\n2: Search Student\n3: Delete Student Record\n4: List top 03 of class\n5: Mark student attendance\n6: View attendance");
+                user_option = Convert.ToInt16(Console.ReadLine());
+                switch (user_option)
+                {
+                    case 1:
+                        string student_ID, student_Name, student_Semester, student_CGPA, student_Department, student_University;
+                        Console.WriteLine("Enter Student ID : ");
+                        student_ID = Console.ReadLine();
+                        FileStream file_Read;
+                        string linee;
+                        int check = 0;
+                        file_Read = new FileStream("E:\\Student.txt", FileMode.Open, FileAccess.Read);
+                        StreamReader read = new StreamReader(file_Read);
+                        while ((line = read.ReadLine()) != null)
+                        {
+                            if (line == "ID : "+student_ID)
+                            {
+                                check++;
+                                break;
+                            }
+                        }
+                        file_Read.Close();
+                        if (check > 0)
+                        {
+                            Console.WriteLine("This ID Already In Use");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter Student Name : ");
+                            student_Name = Console.ReadLine();
+                            Console.WriteLine("Enter Student Semester : ");
+                            student_Semester = Console.ReadLine();
+                            Console.WriteLine("Enter Student CGPA : ");
+                            student_CGPA = Console.ReadLine();
+                            Console.WriteLine("Enter Student Department : ");
+                            student_Department = Console.ReadLine();
+                            Console.WriteLine("Enter Student University : ");
+                            student_University = Console.ReadLine();
+                            student_Object.New_Student(student_ID, student_Name, student_Semester, student_CGPA, student_Department, student_University);
+                            Console.WriteLine("Srudent Added");
+                        }
+                        break;
+
+                    case 2:
+                        int option;
+                        Console.WriteLine("1: Search by ID\n2: Search by Name\n3: Search by Semester");
+                        option = Convert.ToInt16(Console.ReadLine());
+                        if (option == 1)
+                        {
+                            Console.WriteLine("Enter Student ID : ");
+                            student_ID = Console.ReadLine();
+                            student_Object.search_By_Id(student_ID);
+                        }
+                        else if (option == 2)
+                        {
+                            Console.WriteLine("Enter Student Name : ");
+                            student_Name = Console.ReadLine();
+                            student_Object.search_By_Name(student_Name);
+                        }
+                        else if (option == 3)
+                        {
+                            Console.WriteLine("Enter Student Semester : ");
+                            student_Semester = Console.ReadLine();
+                            student_Object.search_By_Semester(student_Semester);
+                        }
+
+                        break;
+
+                    case 3:
+                        string id;
+                        Console.WriteLine("Enter Student ID ");
+                        id=Console.ReadLine();
+                        student_Object.delete_Student(id);
+                        break;
+
+                    case 4:
+                        string semester;
+                        Console.WriteLine("Enter Semester ");
+                        semester = Console.ReadLine();
+                        student_Object.top_Student_By_Semester(semester);
+                        break;
+
+                    case 5:
+                        string name,attendance,semesterr;
+                        Console.WriteLine("Enter Semester");
+                        semesterr = Console.ReadLine();
+                        Console.WriteLine("Enter Student Name ");
+                        name = Console.ReadLine();
+                        Console.WriteLine("presence? (yes or no)");
+                        attendance = Console.ReadLine();
+                        student_Object.mark_attendance(name,attendance,semesterr);
+                        break;
+                    case 6:
+                        string semster;
+                        Console.WriteLine("Enter semester");
+                        semster = Console.ReadLine();
+                        student_Object.view_Attendance(semster);
+                        break;
+                }
+            }
+
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ student profile/student profile/Student.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace student_profile
+{
+    class Student
+    {
+
+        
+        FileStream file_Read;
+        FileStream file_Write;
+        public Student()
+        {
+            
+           
+        }
+       
+        public void New_Student(string id,string name,string semester,string cgpa,string department,string university)
+        {
+            file_Write = new FileStream("E:\\Student.txt", FileMode.Append, FileAccess.Write);
+            byte[] newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            newline = null;
+            byte[] buffer= Encoding.ASCII.GetBytes("ID : "+id);
+            file_Write.Write(buffer, 0, buffer.Length);
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            buffer = null;
+            newline = null;
+            buffer = Encoding.ASCII.GetBytes("Name : "+name);
+            file_Write.Write(buffer, 0, buffer.Length);
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            buffer = null;
+            newline = null;
+            buffer = Encoding.ASCII.GetBytes("Semester : "+semester);
+            file_Write.Write(buffer, 0, buffer.Length);
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            buffer = null;
+            newline = null;
+            buffer = Encoding.ASCII.GetBytes("CGPA : "+cgpa);
+            file_Write.Write(buffer, 0, buffer.Length);
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            buffer = null;
+            newline = null;
+            buffer = Encoding.ASCII.GetBytes("Department : "+department);
+            file_Write.Write(buffer, 0, buffer.Length);
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            buffer = null;
+            newline = null;
+            buffer = Encoding.ASCII.GetBytes("University : "+university);
+            file_Write.Write(buffer, 0, buffer.Length);
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            newline = Encoding.ASCII.GetBytes("Detail End For Above Student");
+            file_Write.Write(newline, 0, newline.Length);
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            file_Write.Close();
+        }
+        public void search_By_Id(string id)
+        {
+            file_Read = new FileStream("E:\\Student.txt", FileMode.Open, FileAccess.Read);
+            StreamReader read = new StreamReader(file_Read);
+            int counter = 0;
+            string line; 
+            
+            while ((line = read.ReadLine()) != null)
+            {
+                if(line=="ID : "+id)
+                {
+                    while((line=read.ReadLine()) != "Detail End For Above Student")
+                    {
+                        Console.WriteLine(line);
+                    }
+                    break;
+                }
+                counter++;
+            }
+
+            read.Close();
+        }
+        public void search_By_Name(string name)
+        {
+            file_Read = new FileStream("E:\\Student.txt", FileMode.Open, FileAccess.Read);
+            StreamReader read = new StreamReader(file_Read);
+            int counter = 0;
+            string line;
+
+            while ((line = read.ReadLine()) != null)
+            {
+                if (line == "Name : " + name)
+                {
+                    Console.WriteLine(line);
+                    while ((line = read.ReadLine()) != "Detail End For Above Student")
+                    {
+                        Console.WriteLine(line);
+                    }
+                    
+                }
+                counter++;
+            }
+
+            read.Close();
+        }
+        public void search_By_Semester(string semester)
+        {
+            file_Read = new FileStream("E:\\Student.txt", FileMode.Open, FileAccess.Read);
+            StreamReader read = new StreamReader(file_Read);
+            int counter = 0;
+            string line;
+            string[] temp = new string[100];
+            int i = 0;
+
+
+            while ((line = read.ReadLine()) != null)
+            {
+                temp[i] = line;
+                if (line == "Semester : " + semester)
+                {
+                    Console.WriteLine(temp[i-2]);
+                    Console.WriteLine(temp[i - 1]);
+                    Console.WriteLine(line);
+                    while ((line = read.ReadLine()) != "Detail End For Above Student")
+                    {
+                        Console.WriteLine(line);
+                    }
+                    
+                    Console.WriteLine("\n\n");
+                }
+                i++;
+                counter++;
+                
+
+            }
+            temp = null;
+            
+            read.Close();
+        }
+
+        public void delete_Student(string id)
+        {
+            string tempFile = Path.GetTempFileName();
+
+            using (var read = new StreamReader("E:\\Student.txt"))
+            using (var write = new StreamWriter(tempFile))
+            {
+                string line;
+
+                while ((line = read.ReadLine()) != null)
+                {
+                    if (line == "ID : "+id)
+                    {
+                        while ((line = read.ReadLine()) != "Detail End For Above Student")
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        write.WriteLine(line);
+                    }
+                        
+                }
+            }
+
+            File.Delete("E:\\Student.txt");
+            File.Move(tempFile, "E:\\Student.txt");
+        }
+        public void top_Student_By_Semester(string semester)
+        {
+            file_Read = new FileStream("E:\\Student.txt", FileMode.Open, FileAccess.Read);
+            StreamReader read = new StreamReader(file_Read);
+            string line;
+            int i = 0;
+            while ((line = read.ReadLine()) != null)
+            {
+                if (line == "Semester : " + semester)
+                {
+                    line = read.ReadLine();
+                    
+                    i++;
+                }
+            }
+            /*for(int j=0;j<ary.Length;j++)
+            {
+                Console.WriteLine(ary[j]);
+            }*/
+            read.Close();
+        }
+        public void mark_attendance(string name,string attendance,string semester)
+        {
+           
+            FileStream file_Write;
+            file_Write = new FileStream("E:\\Attendance.txt", FileMode.Append, FileAccess.Write);
+            byte[] newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            newline = null;
+            byte[] buffer = Encoding.ASCII.GetBytes(semester);
+            file_Write.Write(buffer, 0, buffer.Length);
+            buffer = null;
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            newline = null;
+            buffer = Encoding.ASCII.GetBytes(name);
+            file_Write.Write(buffer, 0, buffer.Length);
+            buffer = null;
+            newline = Encoding.ASCII.GetBytes(Environment.NewLine);
+            file_Write.Write(newline, 0, newline.Length);
+            newline = null;
+            buffer = Encoding.ASCII.GetBytes(attendance);
+            file_Write.Write(buffer, 0, buffer.Length);
+            buffer = null;
+            file_Write.Close();
+            Console.WriteLine("Attendance updated!");
+        }
+        public void view_Attendance(string semester)
+        {
+            file_Read = new FileStream("E:\\Attendance.txt", FileMode.Open, FileAccess.Read);
+            StreamReader read = new StreamReader(file_Read);
+            string line;
+            while ((line = read.ReadLine()) != null)
+            {
+                if (line == semester)
+                {
+                    Console.WriteLine(read.ReadLine());
+                    line = read.ReadLine();
+                    if (line == "yes")
+                        Console.WriteLine("Present");
+                    else
+                        Console.WriteLine("Absent");
+                }
+            }
+            read.Close();
+        }
+    }
+}
